c21e4972b94728f2a38f451c5ddbef3d67f1e85a
Oleg Mukhachev <omukhachev.exceedteam@mail.ru>
2020-08-18 17:41:35 +0300

    commit search-string

--- 1. Basic/5) search-string/index.js
+++ 1. Basic/5) search-string/index.js
@@ -1,5 +1,11 @@
 function searchString(obj) {
-  // Напиши свой код здесь
+    let result = '?';
+    for ( let key in obj) {
+        if (obj[key] !== null && obj[key] !== undefined && obj[key] !== '') result += key + '=' + obj[key] + "&";
+    }
+    if (result === "?") result = "";
+    else  result = result.substr(0,result.length-1);
+    return result;
 }
 
 window.searchString = searchString;
