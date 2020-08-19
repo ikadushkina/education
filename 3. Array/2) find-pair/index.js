618f117e2c054d169efcb967c22aa621b45eabb1
Irina <kadushkina.exceedteam@yandex.ru>
2020-08-19 16:07:44 +0300

    find-pair-fixes

--- 3. Array/2) find-pair/index.js
+++ 3. Array/2) find-pair/index.js
@@ -1,6 +1,14 @@
 function findPair(arr) {
-  // Напиши свой код здесь
-};
+    let x = 0;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i+1; j < arr.length; j++) {
+            if (arr[i] === arr[j])
+                x = arr[i];
+        }
+    }
+    if (x !== 0) return x;
+    else return null;
+}
 
 window.findPair = findPair;
 
