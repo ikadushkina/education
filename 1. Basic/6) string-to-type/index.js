ce3e3e2f1e86200556447cc208f8182c2bbe089d
Irina <kadushkina.exceedteam@yandex.ru>
2020-08-19 10:38:58 +0300

    string-to-type-fixes

--- 1. Basic/6) string-to-type/index.js
+++ 1. Basic/6) string-to-type/index.js
@@ -1,6 +1,23 @@
 function stringToType(str) {
-  // Напиши свой код здесь
-};
+    if (str === "") str = '';
+    else {
+        if (str === "true") str = true;
+        else {
+            if (str === "false") str = false;
+            else {
+                if (str === "null") str = null;
+                else {
+                    if (str === "undefined") str = undefined;
+                    else {
+                        if (!isNaN(Number(str))) str = Number(str);
+                    }
+                }
+            }
+        }
+    }
+    return str;
+
+}
 
 window.stringToType = stringToType;
 
