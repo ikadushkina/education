eecab25c33f1601b7ad6b1f98bfcfd34f95fd409
Irina <kadushkina.exceedteam@yandex.ru>
2020-08-19 16:49:08 +0300

    find-pair-fixes

--- 3. Array/2) find-pair/index.js
+++ 3. Array/2) find-pair/index.js
@@ -1,10 +1,8 @@
 function findPair(arr) {
-    mark:
         for (let i = 0; i < arr.length; i++) {
             for (let j = i + 1; j < arr.length; j++) {
                 if (arr[i] === arr[j]) {
                     return arr[i];
-                    break mark;
                 }
             }
         }
