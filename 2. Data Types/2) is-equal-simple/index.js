8dd1953bec0afc71b867379b4c120c68da2d7bf9
Irina <kadushkina.exceedteam@yandex.ru>
2020-08-19 11:52:04 +0300

    is-equal-simple-fixes

--- 2. Data Types/2) is-equal-simple/index.js
+++ 2. Data Types/2) is-equal-simple/index.js
@@ -1,5 +1,14 @@
 function isEqual(a, b) {
-  // Напиши свой код здесь
+    let a1 = "", b1 = "";
+    if (a && b !== null) {
+        for (let key in a) {
+            a1 += key + ":" + a[key];
+        }
+
+        for (let key in b)
+            b1 += key + ":" + b[key];
+        return a1 === b1;
+    } else return false;
 }
 
 window.isEqual = isEqual;
