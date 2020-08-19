0e22b90c10f57914c3a35a136edc0bd406fd4f21
Irina <kadushkina.exceedteam@yandex.ru>
2020-08-19 17:22:57 +0300

    string-to-type-fixes

--- 1. Basic/6) string-to-type/index.js
+++ 1. Basic/6) string-to-type/index.js
@@ -1,43 +1,21 @@
 function stringToType(str) {
- /*   if (str === "") str = "";
-    else {
-        if (str === "true") str = true;
-        else {
-            if (str === "false") str = false;
-            else {
-                if (str === "null") str = null;
-                else {
-                    if (str === "undefined") str = undefined;
-                    else {
-                        if (!isNaN(Number(str))) str = Number(str);
-                    }
-                }
-            }
+    switch (str) {
+        case "":
+            return "";
+        case "true":
+            return true;
+        case "false":
+            return false;
+        case "null":
+            return null;
+        case "undefined":
+            return undefined;
+        default: {
+            if (!isNaN(Number(str))) return Number(str);
+            else return str;
         }
     }
-
-    return str;
-   */
-   switch (str) {
-       case "":
-           return "";
-       case "true":
-           return true;
-       case "false":
-           return false;
-       case "null":
-           return null;
-       case "undefined":
-           return undefined;
-
-   }
-   switch (!isNaN(Number(str))){
-        case true: return Number(str);
-        default: return str;
-    }
-
 }
-
 window.stringToType = stringToType;
 
 export default stringToType;
