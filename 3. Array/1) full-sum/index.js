57b4c813450f0b6a37d58238dab81163cb7311c6
Irina <kadushkina.exceedteam@yandex.ru>
2020-08-19 18:18:16 +0300

    full-sum-fixes

--- 3. Array/1) full-sum/index.js
+++ 3. Array/1) full-sum/index.js
@@ -1,7 +1,12 @@
 function fullSum(...args) {
-  // Напиши свой код здесь
-}
+    if (!args.length) return 0;
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
+    if (args.every(elem => typeof elem === "number")) {
+        return args.reduce(reducer);
+    }
+    throw new Error("Wrong Argument Type");
+}
 window.fullSum = fullSum;
 
 export default fullSum;
