39ee6eb87887e8642913f4f8026655ae42ade808
Irina <kadushkina.exceedteam@yandex.ru>
2020-08-20 14:30:32 +0300

    xhr-get-fixes

--- 4. Async/2) xhr-get/index.js
+++ 4. Async/2) xhr-get/index.js
@@ -1,5 +1,17 @@
 function xhrGet(url) {
-  // Напиши свой код здесь
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.responseType = 'json';
+        xhr.send();
+        xhr.onload = function () {
+            if (xhr.status === 200 || xhr.status === 201) {
+                resolve(xhr.response);
+            }
+            reject(xhr.response);
+        }
+    })
 }
 
 window.xhrGet = xhrGet;
