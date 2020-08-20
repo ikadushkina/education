e7f4cfcb1d7d0f487e602ca9d10c17db69e56990
Irina <kadushkina.exceedteam@yandex.ru>
2020-08-20 12:58:30 +0300

    async-sum-fixes

--- 4. Async/1) async-sum/index.js
+++ 4. Async/1) async-sum/index.js
@@ -1,10 +1,12 @@
 function asyncSum(a, b) {
-    return new Promise(function (resolve, reject) {
+   return  new Promise(function (resolve, reject) {
+       setTimeout(() =>{
         if (typeof a === "number" && typeof b === "number") {
             resolve(a + b);
         }
         reject("error");
-    });
+    }, 1000);
+});
 }
 
 window.asyncSum = asyncSum;
