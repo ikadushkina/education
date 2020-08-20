aee44ab9d05bc2bb5557caeeadc0fc023936b3fd
Irina <kadushkina.exceedteam@yandex.ru>
2020-08-20 17:12:55 +0300

    arrays-sort-fixes

--- 3. Array/3) arrays-sort/index.js
+++ 3. Array/3) arrays-sort/index.js
@@ -1,24 +1,18 @@
 function arraysSort(arr) {
-    let sortArr;
-        let sort = (a, b) => {
-            if (a[1] > b[1]) return 1;
-            else if (a[1] < b[1]) return -1;
-            else return 0;
+    let max = arr => {
+        let elemMax = 0;
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] > elemMax) elemMax = arr[i];
         }
-    sortArr = arr.sort(sort);
-    return sortArr;
-
-    // let elemMax = -Infinity;
-    // let sortArr = [[]];
-    // for (let i = 0; i < arr.length; i++) {
-    //     for (let j = 0; j < arr.length; j++) {
-    //         if (arr[i][j] > elemMax) {
-    //             elemMax = arr[i];
-    //             sortArr.push(arr[i]);
-    //         }
-    //     }
-    // }
-    // return sortArr;
+        return elemMax;
+    }
+    let sort = (a, b) => {
+        if (max(a) > max(b)) return 1;
+        if (max(a) < max(b)) return -1;
+        return 0;
+    }
+    let newArr = [...arr]
+    return newArr.sort(sort);
 }
 
 window.arraysSort = arraysSort;
