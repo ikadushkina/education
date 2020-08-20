3709b33dbe9e4dea02f93c45aea6dd448ee40a87
Irina <kadushkina.exceedteam@yandex.ru>
2020-08-20 12:46:27 +0300

    async-sum-fixes

--- 4. Async/1) async-sum/index.js
+++ 4. Async/1) async-sum/index.js
@@ -1,10 +1,12 @@
 function asyncSum(a, b) {
-  return  new Promise(function (resolve, reject){
-        if ( typeof a === "number" && typeof b === "number")
-            resolve (a + b);
-         reject("error");
+    return new Promise(function (resolve, reject) {
+        if (typeof a === "number" && typeof b === "number") {
+            resolve(a + b);
+        }
+        reject("error");
     });
 }
+
 window.asyncSum = asyncSum;
 
 export default asyncSum;
