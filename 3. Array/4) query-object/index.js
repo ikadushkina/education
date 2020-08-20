aabe7fd29282159831d8cf2419350269a8acf930
Irina <kadushkina.exceedteam@yandex.ru>
2020-08-20 17:57:03 +0300

    query-object-fixes

--- 3. Array/4) query-object/index.js
+++ 3. Array/4) query-object/index.js
@@ -1,11 +1,59 @@
 function queryToObject(query) {
-let str;
-    if (query.length !==    1) {
-        for (let i = 1; i <= query.length; i++)
-            str = query[i];
+     if (!query.length || query === "?") return {};
+    // var result = {};
+    // var a = "a=true&b=false&c=123&d=0".split('&').map(i => i.split("="))
+    // console.log(a)
+    // a.forEach((item) => {
+    //     result[item[0]]= item[1]
+    // })
+    // console.log('result', result)
+    //
+    // let result = query.split("").map((item) => {
+    //     if (item === '?') return '';
+    //     if (item === "=") return ":"
+    //     if (item === "&") return ","
+    //     return item;
+    // })
+    let arr = query.split("");
+    let string = "";
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === "?") {
+            arr[i] = "";
+            string += arr[i];
+        } else if (arr[i] === "=") {
+            arr[i] = "\":\"";
+            string += arr[i];
+        } else if (arr[i] === "&") {
+            arr[i] = "\",\"";
+            string += arr[i];
+        } else string += arr[i];
     }
-    else  if (query === "" || query === "?") return {};
-    return JSON.parse(str);
+
+    let obj = JSON.parse('{ "' + string + '"}');
+     console.log(obj);
+    for(let key in obj) {
+        switch (obj[key]) {
+            case "":
+                obj[key] = "";
+                break;
+            case "true":
+                obj[key] = true;
+                break;
+            case "false":
+                obj[key] = false;
+                break;
+            case "null":
+                obj[key] = null;
+                break;
+            case "undefined":
+                obj[key] = undefined;
+                break;
+            default: {
+                if (!isNaN(Number(obj[key]))) obj[key] = Number(obj[key]);
+            }
+        }
+    }
+    return obj;
 }
 
 window.queryToObject = queryToObject;
