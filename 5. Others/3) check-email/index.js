a12e06cac98cebe1fdcdeab1204eb8e82ddba8fc
Irina <kadushkina.exceedteam@yandex.ru>
2020-08-21 11:14:15 +0300

    check-email-fixes

--- 5. Others/3) check-email/index.js
+++ 5. Others/3) check-email/index.js
@@ -1,6 +1,6 @@
 function isValidEmail(email) {
-    let reg = /^([A-Za-z0-9_\-.])+@gmail.com$/;
-    return reg.test(email);
+  let reg = /^([\w\-.])+@([\w\-.])+\.([a-z]{2,3})$/i;
+  return reg.test(email);
 }
 
 window.isValidEmail = isValidEmail;
