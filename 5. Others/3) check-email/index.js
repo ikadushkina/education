eccaadfed0828e64d47a0b8417f515a22f5cf395
Irina <kadushkina.exceedteam@yandex.ru>
2020-08-21 10:58:04 +0300

    check-email-fixes

--- 5. Others/3) check-email/index.js
+++ 5. Others/3) check-email/index.js
@@ -1,19 +1,6 @@
-/*
-  Задача
-  Написать функцию которая проверяет, является ли email валидным
-  Прмеры
-  isValidEmail("tima1702@gmail.com") === true
-  isValidEmail("tima1702@gmail") === false
-  isValidEmail("tima1702@gmail.") === false
-  isValidEmail("tima1702gmail.com") === false
-  isValidEmail("example_emai@gmail.com") === true
-  isValidEmail("example8@gmail.com") === true
-  isValidEmail("e2xample8@gmail.com") === true
-  isValidEmail("e2xa.mple8@gmail.com") === true
- */
-
 function isValidEmail(email) {
-  // Напиши свой код здесь
+    let reg = /^([A-Za-z0-9_\-.])+@gmail.com$/;
+    return reg.test(email);
 }
 
 window.isValidEmail = isValidEmail;
